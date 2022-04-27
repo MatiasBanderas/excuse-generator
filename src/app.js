cd3b06728f4da9ac14224496043168584a5e18a1
MatiasBanderas <matias.banderas@ug.uchile.cl>
2022-04-27 00:28:00 +0000

    crea logica

--- src/app.js
+++ src/app.js
@@ -2,10 +2,25 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function randomizer() {
+  let Who = Math.floor(Math.random() * who.length) + 0;
+  let Action = Math.floor(Math.random() * action.length) + 0;
+  let What = Math.floor(Math.random() * what.length) + 0;
+  let When = Math.floor(Math.random() * when.length) + 0;
+  let excuse =
+    who[Who] + " " + action[Action] + " " + what[What] + " " + when[When];
+  return excuse;
+}
+
+console.log(randomizer());
